3ff33b9eaf336b6426a7b82881f284b6c6993115
rashikshrestha <073bex432.rashik@pcampus.edu.np>
2021-07-06 10:57:32 +0545

    a

---
+++ checkgigecameras.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "GenApi/GenApi.h" //!< GenApi lib definitions.
+#include "gevapi.h"		   //!< GEV lib definitions.
+
+int main()
+{
+    std::cout << "This is main" << std::endl;
+    return 0;
+}
