04ef00ebe8f9c7e6540a59aef9eaa1c9ef0b0bee
rashikshrestha <073bex432.rashik@pcampus.edu.np>
2021-07-06 11:48:28 +0545

    a

--- checkgigecameras.cpp
+++ checkgigecameras.cpp
@@ -9,7 +9,10 @@ int main()
 {
     GEV_DEVICE_INTERFACE pCamera[MAX_CAMERAS] = {0};
 	GEV_STATUS status;
+    GEV_CAMERA_HANDLE handle = NULL;
     int numCamera = 0;
+    UINT32 rectimestampModulo = 0;
+    int type;
 
     status = GevGetCameraList(pCamera, MAX_CAMERAS, &numCamera);
 
@@ -18,6 +21,12 @@ int main()
 	{
 		std::cout << i << " : " << pCamera[i].macLow << std::endl;
 	}
+
+    status = GevOpenCamera(&pCamera[0], GevExclusiveMode, &handle);
+
+    GevGetFeatureValue(handle, "timestampModulo", &type, sizeof(UINT32), &rectimestampModulo);
+	std::cout << "get timestampModulo = " << rectimestampModulo << std::endl;
+
 
 
     std::cout << "End of program !! " << std::endl;
