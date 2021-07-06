0917fe356aae7655eeb1996b567d0b4474f92c9b
rashikshrestha <073bex432.rashik@pcampus.edu.np>
2021-07-06 11:03:31 +0545

    a

--- checkgigecameras.cpp
+++ checkgigecameras.cpp
@@ -3,8 +3,23 @@
 #include "GenApi/GenApi.h" //!< GenApi lib definitions.
 #include "gevapi.h"		   //!< GEV lib definitions.
 
+#define MAX_CAMERAS 256
+
 int main()
 {
-    std::cout << "This is main" << std::endl;
+    GEV_DEVICE_INTERFACE pCamera[MAX_CAMERAS] = {0};
+	GEV_STATUS status;
+    int numCamera = 0;
+
+    status = GevGetCameraList(pCamera, MAX_CAMERAS, &numCamera);
+
+	printf("%d camera(s) on the network\n", numCamera);
+	for (int i = 0; i < numCamera; i++)
+	{
+		std::cout << i << " : " << pCamera[i].macLow << std::endl;
+	}
+
+
+    std::cout << "End of program !! " << std::endl;
     return 0;
 }
