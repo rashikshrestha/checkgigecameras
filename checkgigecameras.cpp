ff4cb8de3bc2d8bb8d647bdfc02ab8122498dbd8
rashikshrestha <073bex432.rashik@pcampus.edu.np>
2021-07-07 14:34:19 +0545

    a

--- checkgigecameras.cpp
+++ checkgigecameras.cpp
@@ -15,7 +15,7 @@ int main()
 
     char recieved_string[100] = {0};
 
-    char* feature[5] = {"timestampModulo","ptpMode","ptpStatus","ptpServoStatus","ptpMasterClockId"};
+    char* feature[6] = {"timestampModulo","TriggerSource","ptpMode","ptpStatus","ptpServoStatus","ptpMasterClockId"};
 
     int feature_size = sizeof(feature)/sizeof(feature[0]);
 
